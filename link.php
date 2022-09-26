c3ffe8d256224152effab36d374eed9281420793
yetto-tools <ergonzalez209@gmail.com>
2022-09-25 18:27:47 -0600

    reparcion de ruta de archivos css y js de cabezeras

--- link.php
+++ link.php
@@ -2,11 +2,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- <title><?php echo strtoupper(seTitlePage());?></title> -->
 <!--  bootstrap local -->
-<link rel="stylesheet" type="text/css" href="/Proyect_FEL/src/bootstrap-5.2.1-dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/Proyecto_FEL/src/bootstrap-5.2.1-dist/css/bootstrap.min.css">
 <!-- js bootstrap -->
-<script type="text/javascript" src="/Proyect_FEL/src/bootstrap-5.2.1-dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="/Proyecto_FEL/src/bootstrap-5.2.1-dist/js/bootstrap.bundle.min.js"></script>
 <!--  css personalizado -->
-<link rel="stylesheet" type="text/css" href="/Proyect_FEL/src/css/style.css">
+<link rel="stylesheet" type="text/css" href="/Proyecto_FEL/src/css/style.css">
 
 
 		
