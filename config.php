4bbc899399eb265c8f70b49e7ea93187353bc436
yetto-tools <ergonzalez209@gmail.com>
2022-09-25 18:24:27 -0600

    add function convert image

--- config.php
+++ config.php
@@ -4,14 +4,14 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'test');
- 
+
 
 try {
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD);
     if (!$db) {
         die("<h2>No conectado: " . $db->error."</h2>");
-    } else { 
+    } else {
         $db_selected = $db->select_db(DB_NAME);
         if (!$db_selected) {
             die ('<h2>Error : ' . $db->error.'  </h2>');
@@ -21,20 +21,23 @@ try {
 catch(Exception $e){
     exit("<h2> Hay Problemas: <br>".$e->getMessage()."</h2>");
 }
-
+// obtener nombre del archivo sin extensio
 function seTitlePage(){
     $opcion = substr($_SERVER['PHP_SELF'],strrpos($_SERVER['PHP_SELF'], '/' )+1);
     return explode(".php", $opcion)[0];
 }
-
-function opcion_activa(){
-  $opcion = substr($_SERVER['PHP_SELF'],strrpos($_SERVER['PHP_SELF'], '/' )+1);
+// mostar ruta seleccionada
+  function opcion_activa(){
+    $opcion = substr($_SERVER['PHP_SELF'],strrpos($_SERVER['PHP_SELF'], '/' )+1);
   return  $menu = array(
-            "opcion" => $opcion, 
+            "opcion" => $opcion,
             "is_active" => 'active',
             "endpoint" => explode(".php", $opcion)[0]
           );
   }
 
+  function convertir_a_img_src($blob,$formato){
+    return "data:".$formato.";base64,".$blob;
+  }
 
 ?>
