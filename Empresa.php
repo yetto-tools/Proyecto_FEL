1998c02c32ddf7cd490cfe4208a7f4be3317e7b5
Erick Orlando EORG. Rashon Gonzalez <PAMERG01@gpamericana.com>
2022-09-27 09:10:31 -0600

    Pantalla Empresas con Funcion de Guardar, Editar

--- Empresa.php
+++ Empresa.php
@@ -3,94 +3,78 @@
 session_start();
 require_once "config.php";
 require_once "session_config.php";
-$sql = "SELECT e.id, e.nit, e.nombre, e.direccion, e.ciudad, e.pais, e.telefono, i.logo, i.formato, i.tamaño  FROM empresa e LEFT JOIN imagen i ON i.id =e.logo_id";
-//$db->query("INSERT into imagen (logo,nombre,tamaño,formato) VALUES ('$toImgBase64','$imagen_nombre','$imagen_tamaño','$imagen_formato')");
 
-// Listar Empresas
-$count=0;
-$stmt = $db->prepare($sql);
-// $stmt->bind_param("i", $id);
-$stmt->execute();
-$result = $stmt->get_result(); // get the mysqli result
-//$titulos = $result->fetch_assoc();
-$empresas = $result->fetch_all(MYSQLI_ASSOC); // fetch data
+
+
+function ListaEmpresas($db){
+  $sql = "SELECT e.id, e.nit, e.nombre, e.direccion, e.municipio, e.departamento, e.telefono, logo FROM empresa e;";
+  //$db->query("INSERT into imagen (logo,nombre,tamaño,formato) VALUES ('$toImgBase64','$imagen_nombre','$imagen_tamaño','$imagen_formato')");
+  
+  // Listar Empresas
+  $count=0;
+  $stmt = $db->prepare($sql);
+  // $stmt->bind_param("i", $id);
+  $stmt->execute();
+  $result = $stmt->get_result(); // get the mysqli result
+  //$titulos = $result->fetch_assoc();
+  $empresas = $result->fetch_all(MYSQLI_ASSOC); // fetch data  
+  return $empresas;
+}
+
+ListaEmpresas($db);
 
 // formulario para guarda
-
-// Procesamiento de los datos del formulario cuando éste se envía
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-  /* Comprueba si el nombre de usuario está vacío */
-  if( isset($_POST["nuevo-nit"]) && isset($_POST["nuevo-empresa"]) && isset($_POST["nueva-direccion"])  ){
-    $id =  isset($_POST["id-empresa"]) ? trim($_POST["id-empresa"]) : NULL;
-    $nit = trim($_POST["nuevo-nit"]);
-    $empresa = trim($_POST["nuevo-empresa"]);
-    $direccion = trim($_POST["nueva-direccion"]);
-    $telefono = trim($_POST["nuevo-telefono"]);
-    $departamento = trim($_POST["nuevo-departamento"]);
-    $municipio = trim($_POST["nuevo-municipio"]);
-
-    echo "$id";
-    if($id >= 1 ){
-      
-      try{
-        //UPDATE `empresa` SET `direccion` = '1 Calle 5-18' WHERE `empresa`.`id` = 6
-        $db->begin_transaction(/*MYSQLI_TRANS_START_READ_ONLY*/);
-        $db->query("UPDATE `empresa` SET `nit` = '$nit', `nombre` = '$empresa',
-         `direccion` = '$direccion', `ciudad` = '$municipio', `pais` = '$departamento' 
-          WHERE `empresa`.`id` = '$id'");
-        $db->commit();
-        //header("location: empresa.php");
-      } catch (Exception $e) {
-        echo $e;
-        $db->rollback();
-        throw $e; 
-      
-      }
-    }
-    else{
-      // Crear una Nueva Empresa si Trae logo
-      if(!isset($_FILES['nuevo-logo'])){
-        //leemos almacemanos los datos en variables
-        $imagen_nombre   = $_FILES["nuevo-logo"]["name"];
-        $imagen_data     = $_FILES["nuevo-logo"]["tmp_name"];
-        $imagen_tamaño   = $_FILES["nuevo-logo"]["size"];
-        $imagen_formato  = $_FILES["nuevo-logo"]["type"];
-        // leemos la imagen y la converitmos en base64
-        $read = fopen($imagen_data, "rb");
-        $buffer = fread($read, $imagen_tamaño);
-        $toImgBase64 = base64_encode($buffer);
-        fclose($read);
-        try{
-  
-          $db->begin_transaction(/*MYSQLI_TRANS_START_READ_ONLY*/);
-          $db->query("INSERT INTO `imagen` (`logo`, `nombre`, `tamaño`, `formato`) 
-             VALUES ('$toImgBase64','$imagen_nombre','$imagen_tamaño','$imagen_formato')");
-            // recuperamos el id de la imagen insertada
-             $logo_id = $db->insert_id;
-            echo $db->insert_id;
-            // insertamos los datos de empresa en la tabla
-            
-            $db->query("INSERT INTO `empresa` 
-            VALUES ('$id', '$nit', '$empresa', '$direccion',  '$municipio',  '$departamento',  '$telefono',  '$logo_id')");
-          $db->commit();
-          echo " commit insert ";
-          header("location: empresa.php");
-        } catch (Exception $e) {
-          echo $e;
-          $db->rollback();
-          throw $e; 
-          
-        }
-      }
-      
-    }
-
-
-
-
-
+if($_POST){
+  $mensaje = "";
+  $id =  trim($_POST["id-empresa"]);
+  $nit = trim($_POST["nuevo-nit"]);
+  $empresa = trim($_POST["nuevo-empresa"]);
+  $direccion = trim($_POST["nueva-direccion"]);
+  $telefono = trim($_POST["nuevo-telefono"]);
+  $departamento = trim($_POST["nuevo-departamento"]);
+  $municipio = trim($_POST["nuevo-municipio"]);  
+  $logo = $_POST["base64-logo"];
+    // hay un id entonces vamos a actualizar datos del registro
+  if($id){
+    // comprobamos que los cambos Obligatorios no esten vacios
+    try{  
+      $db->begin_transaction(/*MYSQLI_TRANS_START_READ_ONLY*/);
+      $query_actualizar = "UPDATE `empresa` SET `nit` = '$nit', `nombre` = '$empresa', `direccion` = '$direccion', `telefono` = '$telefono', `municipio` = '$municipio', `departamento` = '$departamento', `logo`  = '$logo' WHERE `id` = '$id' ";
+      $db->query($query_actualizar);
+      $db->commit();
+      $mensaje = '<div class="alert alert-info alert-dismissible" role="alert">'.
+      "Registro Actualizado Correctamente"
+      . '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
+    } 
+    catch (Exception $e) {
+      $mensaje = '<div class="alert alert-danger alert-dismissible" role="alert">'.
+      $e->getMessage()
+      . '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
+      $db->rollback();
+    } // FIN CATCH
   }
-}
+    // Si no hay ID crear nuevo datos
+  else{
+    try{
+      $db->begin_transaction(/*MYSQLI_TRANS_START_READ_ONLY*/);
+      $query_insertar = "INSERT INTO `empresa` (`nit`, `nombre`, `direccion`,  `telefono`, `municipio`, `departamento`, `logo` )  
+      VALUES ('$nit', '$empresa', '$direccion',  '$telefono', '$municipio',  '$departamento', '$logo');";
+      $mensaje = '<div class="alert alert-success" role="alert">'.$query_insertar.'</div>';
+      $db->query($query_insertar);
+      $db->commit();
+      $mensaje = '<div class="alert alert-success alert-dismissible" role="alert">'.
+      "Registro Creado Correctamente"
+      . '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
+    }
+    catch (Exception $e) {
+      $mensaje = '<div class="alert alert-danger alert-dismissible" role="alert">'.
+      $e->getMessage()
+      . '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
+      $db->rollback();
+    } // FIN CATCH
+  } // FIN ELSE NUEVO REGISTRO
+} // FIN DE POST
+
 ?>
 
 <!DOCTYPE html>
@@ -107,7 +91,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           <div class="shadow-lg p-4 mb-5 bg-white rounded">
             <form  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data" id="form-empresa" class="needs-validation" novalidate>
             <div class="row mb-4">
-
+              <div class="col col-md-12 mb-2 d-flex justify-content-center"><div class="col col-md-8 text-center">
+              <div id="liveAlertPlaceholder"><div></div>
+                <div>
+                  <?php echo empty($mensaje) ? "": $mensaje;  ?></div></div>
+                </div>
+              </div>
               <div class="col col-md-12 mb-4">
                 <div class="row col-inline">
                   <div class="col-md text-start">
@@ -118,14 +107,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                   </div>
                 </div>
               </div>
-
               <div class="col col-md-6">
                 <div class="row">
-                    <label class="col-md-4 col-form-label bg-light"><strong>ID Registro</strong></label>
+                    <label class="col-md-4 col-form-label bg-light" hidden><strong>ID Registro</strong></label>
                     <div class="col-md-6">
                       <div class="row mb-3">
                         <div class="col  pe-0">
-                          <input type="text" id="id-empresa" name="id-empresa" class="form-control form-control-sm" placeholder="ID Registo" value="" readonly disabled>
+                          <input type="hidden" id="id-empresa" name="id-empresa" class="form-control form-control-sm bg-light" placeholder="ID Registo" value="" readonly>
                           <div class="valid-feedback">listo!</div>
                           <div class="invalid-feedback">Campo Obligatorio!!</div>
 
@@ -142,7 +130,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                           <input type="text" id="nuevo-nit" name="nuevo-nit" class="form-control form-control-sm mb-2" placeholder="NIT" required>
                           <div class="valid-feedback">listo!</div>
                           <div class="invalid-feedback">Campo Obligatorio!!</div>
-
                         </div>
                       </div>
                     </div>
@@ -152,10 +139,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     <div class="col-md-6">
                       <div class="row mb-3">
                         <div class="col  pe-0">
-                           <input type="text" id="nuevo-empresa" name="nuevo-empresa" class="form-control form-control-sm mb-2" placeholder="empresa" required>
-                          <div class="valid-feedback">listo!</div>
-                          <div class="invalid-feedback">Campo Obligatorio!!</div>
-
+                            <input type="text" id="nuevo-empresa" name="nuevo-empresa" class="form-control form-control-sm mb-2" placeholder="empresa" required>
+                            <div class="valid-feedback">listo!</div>
+                            <div class="invalid-feedback">Campo Obligatorio!!</div>
                           </div>
                       </div>
                     </div>
@@ -215,6 +201,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                       <div class="row mb-3">
                         <div class="col pe-0">
                           <input type="file" id="nuevo-logo" name="nuevo-logo"  accept="image/png, image/gif, image/jpeg" class="form-control form-control-sm mb-2">
+                          <input type="hidden" id="base64-logo" name="base64-logo" value="">
                           <div class="valid-feedback">listo!</div>
                           <div class="invalid-feedback">Campo Obligatorio!!</div>
                           <div class="text-small">
@@ -256,7 +243,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </tr>
                   </thead>
                   <tbody id="ListaEmpresas" class="fixed_header">
-                    <?php if ($empresas): ?>
+                    <?php if ($empresas= ListaEmpresas($db)): ?>
                         <?php foreach($empresas as $row): ?>
                           <tr id="<?= $count+=1; ?>" name="linea">
                             <th scope="row" name="numero"><?= $count; ?></th>
@@ -265,15 +252,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <td><input type="text" name="empresa" class="form-control form-control-sm" value="<?= $row['nombre']; ?>" disabled readonly required></td>
                             <td hidden><input type="text" name="telefono" class="form-control form-control-sm" value="<?= $row['telefono']; ?>" disabled readonly required></td>
                             <td><textarea name="direccion"  rows="1" cols="90" class="form-control form-control-sm" value="<?= $row['direccion']; ?>"  disabled readonly required><?= $row['direccion']; ?></textarea></td>
-                            <td hidden><input type="text" name="departamento" class="form-control form-control-sm" value="<?= $row['ciudad']; ?>" disabled readonly required></td>
-                            <td hidden><input type="text" name="municipio" class="form-control form-control-sm" value="<?= $row['pais']; ?>" disabled readonly required></td>
-                            <td><img name="logo-preview"  width="48" height="34" src="<?= convertir_a_img_src($row['logo'], $row['formato'])?>"></td>
+                            <td hidden><input type="text" name="departamento" class="form-control form-control-sm" value="<?= $row['departamento']; ?>" disabled readonly required></td>
+                            <td hidden><input type="text" name="municipio" class="form-control form-control-sm" value="<?= $row['municipio']; ?>" disabled readonly required></td>
+                            <td><img name="logo-preview"  width="48" height="34" src="<?=$row['logo'];?>"></td>
                             <th class="text-center"><input type="button" name="editar" class="btn btn-sm btn-warning" value="&#9998;" tokenID="<?= $row['id'] ?>"/></th>
                           </tr>
                         <?php endforeach ?>
                       </tr>
                     <?php else: ?>
-                      No data found
+                      <strong>No data found</strong>
                     <?php endif ?>
                   </tbody>
                 </table>
